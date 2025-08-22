02231b251edb10b92864d5124abb963e8abb0336
github-action-benchmark <github@users.noreply.github.com>
2025-08-22 12:38:13 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for cac43ed3843e100c7a225a5519510c8f8d296595

---
+++ benchmarks/data.js
@@ -0,0 +1,60 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1755866293408,
+  "repoUrl": "https://github.com/Maury1329/n8n-mcp",
+  "entries": {
+    "n8n-mcp Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "56956555+czlonkowski@users.noreply.github.com",
+            "name": "Romuald Członkowski",
+            "username": "czlonkowski"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cac43ed3843e100c7a225a5519510c8f8d296595",
+          "message": "Merge pull request #155 from czlonkowski/update-n8n-dependencies\n\nchore: update n8n dependencies to v1.107.4",
+          "timestamp": "2025-08-20T19:53:10+02:00",
+          "tree_id": "22242f23b6c77f2ac5d007164e37e81514fa536c",
+          "url": "https://github.com/Maury1329/n8n-mcp/commit/cac43ed3843e100c7a225a5519510c8f8d296595"
+        },
+        "date": 1755866293114,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0193,
+            "unit": "ms",
+            "range": 0.2943,
+            "extra": "51918 ops/sec"
+          },
+          {
+            "name": "sample - array sorting - large",
+            "value": 3.2575,
+            "unit": "ms",
+            "range": 0.8869000000000002,
+            "extra": "307 ops/sec"
+          },
+          {
+            "name": "sample - string concatenation",
+            "value": 0.005,
+            "unit": "ms",
+            "range": 0.3035,
+            "extra": "201367 ops/sec"
+          },
+          {
+            "name": "sample - object creation",
+            "value": 0.0668,
+            "unit": "ms",
+            "range": 0.41109999999999997,
+            "extra": "14979 ops/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
